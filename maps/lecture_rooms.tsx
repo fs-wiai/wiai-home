6104d81c721676a1dfc4ebeea8a348808b94f2a8
Fhavyre <40170596+Fhavyre@users.noreply.github.com>
2021-04-18 16:57:12 +0000

    Deploying to gh-pages from master @ 57711408a0246154eac4c35a6e9e98d542b6e8fa 🚀

--- maps/lecture_rooms.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.5.0" name="lecture_rooms" tilewidth="32" tileheight="32" tilecount="400" columns="20">
- <image source="lecture_rooms.png" width="640" height="640"/>
-</tileset>
--- maps/tilesets_deviant_milkian_1.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.5.0" name="tilesets_deviant_milkian_1" tilewidth="32" tileheight="32" tilecount="256" columns="16">
- <image source="tilesets_deviant_milkian_1.png" width="512" height="512"/>
-</tileset>
